4a6b376c3635358e68bdcef431ec907a3fdcf3b1
Arvin <ACRCRIS@chinabank.ph>
2025-05-19 16:55:33 +0800

    React like functionality

--- client_react/src/components/card/Card.jsx
+++ client_react/src/components/card/Card.jsx
@@ -1,7 +1,14 @@
 import './card.css'
-import { Heart, MessageCircle, Send, Info } from 'lucide-react';
+import { Heart, MessageCircle, Send, HeartHandshake  } from 'lucide-react';
+import { useState } from 'react';
 
 const Card = ({post}) => {
+    const [liked, setLiked] = useState(false);
+
+    const handleNotification = () => {
+        setLiked(!liked);
+    }
+
     return (
         <div className="card">
             <div className="info">
@@ -10,7 +17,11 @@ const Card = ({post}) => {
             </div>
             <img src={post.postImg} alt="" className="postImg" />
             <div className="interaction">
-                <Heart/>
+                { liked ? (
+                    <HeartHandshake onClick={handleNotification}/>
+                ) : (
+                    <Heart onClick={handleNotification}/>
+                ) }
                 <MessageCircle/>
                 <Send/>
                 {/* <Info className='infoIcon'/> */}
